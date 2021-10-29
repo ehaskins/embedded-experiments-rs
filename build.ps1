1044eafaadbda3c7ab80dd77f1289bbc0db964ae
Eric Haskins <eric@ehaskins.com>
2021-10-29 15:55:05 -0500

    Linux frindlyizing

--- build.ps1
+++ build.ps1
@@ -1,10 +1,14 @@
+#! /usr/bin/env pwsh
+
+$ErrorActionPreference = "Stop"
+
 Push-Location ./bootloader
 
-cargo build
+cargo build --release
 
 Set-Location ../app
 
-cargo build
+cargo build --release
 cargo objcopy -- -O binary app.bin
 
 Pop-Location
